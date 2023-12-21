9ebeed475b8c0fbb784d3552c63994f33cd312a1
BuildTools <unconfigured@null.spigotmc.org>
2023-12-20 23:54:19 -0300

    Microservices gateway

---
+++ src/main/java/br/com/food/gateway/configuration/GatewayConfig.java
@@ -0,0 +1,9 @@
+package br.com.food.gateway.configuration;
+
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GatewayConfig {
+//
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder)
+//}
